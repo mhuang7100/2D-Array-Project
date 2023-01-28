8864bc5399c1d51aea7d84a9278fa503e6b4ecd6
mhuang7100 <mhuang00@ladueschools.net>
2023-01-28 23:45:57 +0000

    ~

--- Board.java
+++ Board.java
@@ -1,11 +1,25 @@
 public class Board{
     //creates a new board 
     String[][] array = new String[6][7];
+    // stores the Row/Column location of the last piece placed (for purposes of checking if the player won)
+    int prevC;
+    int prevR;
     // clears the board
     public static void main(String[] args){
         Board game = new Board();
-        game.addPiece(0, 1);
+        game.addPiece(1, 1);
+        game.addPiece(2, 0);
+        game.addPiece(3, 0);
+        game.addPiece(4, 0);
+        game.addPiece(3, 0);
+        game.addPiece(4, 0);
+        game.addPiece(2, 1);
+        game.addPiece(3, 1);
+        game.addPiece(4, 0);
+//        game.addPiece(4, 1);
+        
         game.printBoard();
+        game.gameOver();
     }
 
     // makes the board empty
@@ -15,6 +29,8 @@ public class Board{
                 array[i][j] = "[ ]";
             }
         }
+        prevC = 1;
+        prevR = 5;
     } 
 
     //returns the board
@@ -24,6 +40,13 @@ public class Board{
 
     // prints the current state of the board
     public void printBoard(){
+        System.out.print("\033[2J\033[H");
+        // prints column numbers
+        for (int i = 1; i <= 7; i++){
+            System.out.print(" " + i + "  ");
+        }
+        System.out.println();
+        // prints board
         for (String[] list : array){
             for (String a : list){
                 System.out.print(a + " ");
@@ -34,13 +57,20 @@ public class Board{
 
     // adds the corresponding piece to the lowest open slot in the column
     public void addPiece(int column, int player){
+        // makes player input line up with the actual indexes
+        column -= 1;
+        
         for (int i = 5; i >= 0; i--){
             if (array[i][column].equals("[ ]")){
                 if (player == 0){
                     array[i][column] = " 🔴";
+                    prevC = column;
+                    prevR = i;
                     break;
                 } else {
                     array[i][column] = " 🔵";
+                    prevC = column;
+                    prevR = i;
                     break;
                 }
             }
@@ -58,6 +88,68 @@ public class Board{
 
     // FINISH THIS
     public boolean gameOver(){
+        // this var indicates how many checkers are connected 
+        int win = 1;
+        // checks horizontally
+        
+        for (int i = 1; i <= Math.min(6 - prevC, 3); i++){
+            if (array[prevR][prevC].equals(array[prevR][prevC + i])){
+                win += 1;
+            } else break;
+        }
+        for (int i = 1; i <= Math.min(prevC, 3); i++){
+            if (array[prevR][prevC].equals(array[prevR][prevC - i])){
+                win += 1;
+            } else break;
+        }
+        if (win == 4){
+            System.out.println("GAME OVER");
+            return true;
+        }
+        win = 1;
+        
+        
+        // checks vertically
+        if (prevR <= 2){
+            if (array[prevR][prevC].equals(array[prevR + 1][prevC]) && array[prevR][prevC].equals(array[prevR + 2][prevC]) && array[prevR][prevC].equals(array[prevR + 3][prevC])){
+                System.out.println("GAME OVER"); 
+                return true;
+            }
+        }
+        
+        // checks diagonally 
+        
+        for (int i = 1; i <= Math.min(6 - prevC, prevR); i++){
+            if (array[prevR][prevC].equals(array[prevR - i][prevC + i])){
+                win += 1;
+            } else break;
+        }
+        for (int i = 1; i <= Math.min(prevC, 5 - prevR); i++){
+            if (array[prevR][prevC].equals(array[prevR + i][prevC - i])){
+                win += 1;
+            } else break;
+        }
+        if (win == 4){
+            System.out.println("GAME OVER"); 
+            return true;
+        }
+
+        win = 1;
+        for (int i = 1; i <= Math.min(prevC, prevR); i++){
+            if (array[prevR][prevC].equals(array[prevR - i][prevC - i])){
+                win += 1;
+            } else break;
+        }
+        for (int i = 1; i <= Math.min(6 - prevC, 5 - prevR); i++){
+            if (array[prevR][prevC].equals(array[prevR + i][prevC + i])){
+                win += 1;
+            } else break;
+        }
+
+        if (win == 4){
+            System.out.println("GAME OVER"); 
+            return true;
+        }
         
         return false;
     }
--- Main.java
+++ Main.java
@@ -2,72 +2,106 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Board game = new Board();
         Scanner read = new Scanner(System.in);
-        
         // 0 = red, 1 = blue
         int playerTurn = 0;
 
-        //to see if the game should end
         boolean end = false;
+        boolean finalEnd = false;
 
         //which column user wants to put piece in
         String input = "";
-
-        while(end == false){
-            // checks which player to ask
-            if(playerTurn == 0){
-                System.out.println("Which column would you like to drop you piece player red?");
-                input = read.nextLine();
-            }
-            else{
-                System.out.println("Which column would you like to drop you piece player blue?");
-                input = read.nextLine();
-            }
-            
-            while(!notALetter(input) || Integer.parseInt(input) < 0 || Integer.parseInt(input) > 6){
+        while (finalEnd == false){
+            Board game = new Board();
+            game.printBoard();
+            while(end == false){
+                // creates the board and prints it
                 
-                System.out.println("\nThat is invalid.");
+                // checks which player to ask
                 if(playerTurn == 0){
-                    System.out.println("Which column would you like to drop you piece player red?");
+                    System.out.println("Which column would you like to drop your piece player red🔴?");
                     input = read.nextLine();
+
+                    //checks for valid input
+                    while(!validInput(input)){
+                        System.out.println("\nThat is invalid.");
+                        System.out.println("Which column would you like to drop your piece player red🔴?");
+                        input = read.nextLine();
+                    }
+
+                    game.addPiece(Integer.valueOf(input), 0);
                 }
                 else{
-                    System.out.println("Which column would you like to drop you piece player blue?");
+                    System.out.println("Which column would you like to drop your piece player blue🔵?");
                     input = read.nextLine();
-                }
-                // checking specifically
-                int intInput = Integer.parseInt(input);
-                while(!game.columnEmpty(intInput)){
-                    if(playerTurn == 0){
-                        System.out.println("Which column would you like to drop you piece player red?");
+
+                    //checks for valid input
+                    while(!validInput(input)){
+                        System.out.println("\nThat is invalid.");
+                        System.out.println("Which column would you like to drop your piece player blue🔵?");
                         input = read.nextLine();
+                    }
+
+                    game.addPiece(Integer.valueOf(input), 1);
+                }
+                
+                /* 
+                while(!notALetter(input) || Integer.parseInt(input) < 1 || Integer.parseInt(input) > 7){
+                    
+                    System.out.println("\nThat is invalid.");
+                    if(playerTurn == 0){
+                        System.out.println("Which column would you like to drop your piece player red🔴?");
+                        input = read.nextLine();
+                        game.addPiece(Integer.valueOf(input), 0);
                     }
                     else{
-                        System.out.println("Which column would you like to drop you piece player blue?");
+                        System.out.println("Which column would you like to drop your piece player blue🔵?");
                         input = read.nextLine();
+                        game.addPiece(Integer.valueOf(input), 1);
+                    }
+                    // checking specifically
+                    int intInput = Integer.parseInt(input);
+                    while(!game.columnEmpty(intInput)){
+                        if(playerTurn == 0){
+                            System.out.println("Which column would you like to drop your piece player red🔴?");
+                            input = read.nextLine();
+                            game.addPiece(Integer.valueOf(input), 0);
+                        }
+                        else{
+                            System.out.println("Which column would you like to drop your piece player blue🔵?");
+                            input = read.nextLine();
+                            game.addPiece(Integer.valueOf(input), 1);
+                        }
                     }
                 }
-            }
-        
-
-            int intInput = Integer.parseInt(input);
-            game.addPiece(intInput, playerTurn);
-            
+                */
             
 
+                //int intInput = Integer.parseInt(input);
+                game.printBoard();
 
-            if(playerTurn == 1){
-                playerTurn--;
+                if(playerTurn == 1){
+                    playerTurn--;
+                }
+                else{
+                    playerTurn++;
+                }
+                
+                end = game.gameOver();
+                
+            }   
+            System.out.println("Do you want to play again? [y]/[n]");
+            if (read.nextLine().equals("n")){
+                finalEnd = true;
+            } else {
+                finalEnd = false;
+                end = false;
             }
-            else{
-                playerTurn++;
-            }
-            
         }
 
     }
 
+    /* 
     public static boolean notALetter(String input){
         try{
             int temp = Integer.parseInt(input);
@@ -76,5 +110,22 @@ public class Main {
         catch(Exception e){
             return false;
         }
+    }
+    */
+
+    public static boolean validInput(String input){
+        int temp = 0;
+        //checking so it's a number
+        try{
+            temp = Integer.parseInt(input);
+        }
+        catch(Exception e){
+            return false;
+        }
+        //checking so number is in range
+        if(temp < 1 || temp > 7){
+            return false;
+        }
+        return true;
     }
 }
